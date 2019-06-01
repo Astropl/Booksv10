7a9d57f12c693f094fb0bd3ea495282095322679
Pawel <martys.pawel@gmail.com>
2019-06-01 12:56:57 +0200

    3

--- src/main/java/astropl/App.java
+++ src/main/java/astropl/App.java
@@ -7,19 +7,21 @@ import java.util.Scanner;
  */
 public class App {
     public static void main(String[] args) {
+        String name, lastName;
         Scanner sc = new Scanner(System.in);
 
 
         System.out.println("Podaj tytuł ksiązki");
         String title = sc.nextLine();
-
-        System.out.println("Podaj autora ksiązki");
-        String author = sc.nextLine();
+        Author author = new Author();
+        author.authorInit();
+        //System.out.println("Podaj autora ksiązki");
+        //String author = sc.nextLine();
         System.out.println("Hello World!");
         //Book book = new Book(title, author);
         //book.printbook();
-        Books books = new Books(title,author);
-        System.out.println (books);
+        Books books = new Books(title, author);
+        System.out.println(books);
 
     }
 }
--- src/main/java/astropl/Author.java
+++ src/main/java/astropl/Author.java
@@ -1,4 +1,60 @@
 package astropl;
 
+import java.util.Scanner;
+
 public class Author {
+
+    private String name, lastName;
+
+    Scanner sc = new Scanner(System.in);
+    private Author author;
+
+    public Author() {
+    }
+
+    public void authorInit() {
+        System.out.println(" Podaj imię autora: ");
+        name = sc.nextLine();
+        System.out.println(" Podaj nazwisko autora: ");
+        lastName = sc.nextLine();
+        addAuthor(name, lastName);
+    }
+
+
+    public Author(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Author addAuthor(String name, String lastName) {
+        author.setName(name);
+        author.setLastName(lastName);
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+
+                ", author=" + author +
+                '}';
+    }
 }
